1b643bedfd64a496293cc1258579693a5a6400cd
mathias-crochet <mathias.crochet@estiam.com>
2024-04-20 20:32:51 +0200

    feat: :sparkles: add get all request

--- apis/crud/index.js
+++ apis/crud/index.js
@@ -1,14 +1,43 @@
 require('./database/connexion');
 const User = require('./model/schema');
+const cors = require('cors');
 
 const express = require('express');
 
 const app = express();
 app.use(express.json());
 
-app.get('/', function (req, res) {
-    res.send('hello world');
+app.use(
+    cors({
+        origin: [
+            "http://localhost : 3000",
+        ],
+        credentials: true,
+    })
+);
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
+
+app.get('/users/get', async (req, res) => {
+    console.log('toto');
+    try {
+        const users = await User.find()
+
+        if (!users) {
+            return res.status(404).json({ error: 'Utilisateur non trouvé' });
+        }
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('Error whule getting users: ', error);
+        res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des utilisateurs' });
+    }
+})
 
 app.get('/users/:userId', async (req, res) => {
     try {
@@ -22,7 +51,7 @@ app.get('/users/:userId', async (req, res) => {
 
         res.status(200).json(user);
     } catch (error) {
-        console.error('Erreur lors de la récupération de l\'utilisateur:', error);
+        console.error(`Error while getting user with id "${userId}" : ${error}`);
         res.status(500).json({ error: 'Une erreur est survenue lors de la récupération de l\'utilisateur' });
     }
 });
@@ -58,12 +87,12 @@ app.put('/users/update/:userId', async (req, res) => {
         const updatedUser = await User.findByIdAndUpdate(userId, newData, { new: true });
 
         if (!updatedUser) {
-            return res.status(404).json({ error: 'Utilisateur non trouvé' });
+            return res.status(404).json({ error: 'User not found' });
         }
 
         res.status(200).json(updatedUser);
     } catch (error) {
-        console.error('Erreur lors de la mise à jour de l\'utilisateur:', error);
+        console.error('Error while updating the user: ', error);
         res.status(500).json({ error: 'Une erreur est survenue lors de la mise à jour de l\'utilisateur' });
     }
 });
@@ -74,12 +103,12 @@ app.delete('/users/delete/:userId', async (req, res) => {
         const deletedUser = await User.findByIdAndDelete(userId);
 
         if (!deletedUser) {
-            return res.status(404).json({ error: 'Utilisateur non trouvé' });
+            return res.status(404).json({ error: 'User not found' });
         }
 
         res.status(200).json({ message: 'Utilisateur supprimé avec succès' });
     } catch (error) {
-        console.error('Erreur lors de la suppression de l\'utilisateur:', error);
+        console.error('Error while deleting user :', error);
         res.status(500).json({ error: 'Une erreur est survenue lors de la suppression de l\'utilisateur' });
     }
 });
