dc5f6b357464317c31ad24d55fb222ec4cca7444
el-hadji-mamadou-sarr <elhadji.sarr@outlook.com>
2024-04-21 17:05:20 +0200

    add gateway

--- apis/auths/models/User.js
+++ apis/auths/models/User.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-const User = mongoose.model("USERS", UserSchema);
+const User = mongoose.model("users", UserSchema);
 module.exports = User;
